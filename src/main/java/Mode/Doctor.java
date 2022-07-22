4a3a091c0c5fa0c1e37ca340526b87d19093c1ff
dieudonne <dieudonnetsongo@gmail.com>
2022-07-22 16:22:54 +0300

    Initial commit

---
+++ src/main/java/Mode/Doctor.java
@@ -0,0 +1,2 @@
+package Mode;public class Doctor {
+}
---
+++ src/main/java/Mode/Patient.java
@@ -0,0 +1,16 @@
+package Mode;
+
+public class Person {
+    String f_name;
+    String l_name;
+    String gender;
+    String phone;
+
+    public void login(){
+
+    }
+    public void logout(){
+
+    }
+
+}
